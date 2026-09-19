241d69f1d906a979434ef6aa3876be781cc883ed
Siva <pmrlsivas@gmail.com>
2026-09-19 22:19:52 +0530

    db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,6 @@ class DatabaseSeeder extends Seeder
         $sources = ['Justdial', 'Meta Ads', 'Website', 'WhatsApp', 'Referral', 'Walk-in', 'IndiaMART', 'Google Ads', 'Other'];
         foreach ($sources as $index => $name) LeadSource::create(['name' => $name, 'color' => ['#F4B942','#4DA3A7','#E9775B','#78A6D8'][$index % 4]]);
         foreach (['Website', 'Mobile App', 'SEO', 'Digital Marketing', 'Billing Software', 'Custom Software'] as $name) Service::create(['name' => $name]);
-        User::factory()->create(['name' => 'Aarav Mehta', 'email' => 'admin@example.com', 'password' => 'password']);
+        User::create(['name' => 'Aarav Mehta', 'email' => 'admin@example.com', 'password' => 'password']);
     }
 }
